b09fe47a2edcffb6d5fe0e20a1c0694d1ea881f5
pronesj0293 <160407232+pronesj0293@users.noreply.github.com>
2024-02-18 06:28:56 -0800

    Add files via upload
    P1LAB1

---
+++ P1LAB1_PronesJordan.py
@@ -0,0 +1,13 @@
+    #P1LAB1
+
+    #Your Name
+your_name = input('Name:')
+
+    #Date: 13 Feb 2024
+    
+    #Assignment Name: P1LAB1
+
+    #A brief description of the project: This is an introduction to Python project that will incorporate the input and print functions.
+
+print('Hey', your_name,
+      '\nWelcome to zyBooks!')
